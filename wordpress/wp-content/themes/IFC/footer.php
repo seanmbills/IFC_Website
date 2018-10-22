81cd1bd81f1531d1aa2415fd17861538bd9f69c2
Sean Bills <sean.bills@gtri.gatech.edu>
2018-10-22 11:24:51 -0400

    Created and populated the header.php, footer.php, index.php, and style.css files. Created functions.php and will populate soon

---
+++ wordpress/wp-content/themes/IFC/footer.php
@@ -0,0 +1,59 @@
+        <div class="row">
+            <div class="col-lg-12">
+                <hr>
+            </div>
+        </div>
+
+        
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p style="color:#263975; text-align:center">Questions? Comments? Concerns? Please feel free to contact our IFC President, Jack Corelli, as well as our Georgia Tech Greek Affairs Director, Jamison Keller, by clicking the "Contact Us" button!</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-default btn-block contact-us-button" href="mailto:jamison.keller@studentlife.gatech.edu,president@ifc.greek.gatech.edu"  style="color:#FF2400; background-color:white;">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-12" style="text-align:center;">
+                    <a href="https://www.facebook.com/gtifc/" class="footer-social-media-icons" style="background-color:#4468b0!important; color:white!important;">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a href="https://www.instagram.com/gt_ifc/" class="instagram footer-social-media-icons">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                    <a href="https://twitter.com/GT_IFC" class="footer-social-media-icons" style="background-color:#2aa3ef!important; color:white!important;">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Footer -->
+        <footer >
+            <div class="row" style="background:url(); background-repeat:no-repeat; background-position:center; padding:0px 0 70px 0; margin:0 0 0 0;">
+                <div class="col-md-12">
+                    <p align="center">Copyright &copy; 2018 <strong>Interfraternity Council at Georgia Institute of Technology</strong></p>
+                </div>
+            </div>
+        </footer>
+
+
+        <!-- import all necessary base class javascript files -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+
+
+
+</body>
+
+</html>
---
+++ wordpress/wp-content/themes/IFC/index.php
@@ -0,0 +1,104 @@
+<!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('./img/carousel/Wreck Photo Exec.png'); background-position:center bottom;"></div>
+                <div class="carousel-caption">
+                    <h1 style="text-shadow:black 0px 0px 10px;">Georgia Tech IFC</h1>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('./img/carousel/Sigma Chi Puppy.png'); background-position:center center;"></div>
+                <div class="carousel-caption">
+                    <h2 style="text-shadow:black 0px 0px 10px;"></h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('./img/carousel/Chi Phi.png'); background-position: center top;"></div>
+                <div class="carousel-caption">
+                    <h2 style="text-shadow:black 0px 0px 10px;"></h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('./img/carousel/SigEp Flag.png');"></div>
+                <div class="carousel-caption">
+                    <h2 style="text-shadow:black 0px 0px 10px;"></h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <i class="glyphicon glyphicon-chevron-left"></i>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <i class="glyphicon glyphicon-chevron-right"></i>
+        </a>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container" z-index:2000>
+        <hr>
+
+        <div class="row">
+            <div class="col-md-4 col-sm-4 what-we-do">
+                <div class="panel panel-default text-center">
+                    <div class="panel-body">
+                        <h3>WHAT WE DO</h3>
+                        <br>
+                        <p>The Interfraternity Council (IFC) at Georgia Tech leads and directs the 32 member fraternities on campus. Each of the thirty-two chapters represented are members of the North-American Interfraternity Conference (NIC), an national governing body. The Interfraternity Council promotes the interests of fraternities in general, and insures cooperation among the different fraternities on campus. 
+                        <br>
+                        <br>
+                        <br>
+                        <b>Mission Statement:</b>
+                        <br>
+                        The mission of the Interfraternity Council is to develop ideal members of society who lead with integrity, encourage outstanding academic performance, support the development of our member chapters, promote the traditions and growth of the Greek community, and set a standard of excellence for all students at the Georgia Institute of Technology. Click here to learn more about IFC's role on campus and for some important IFC resources!</p>
+                        <br>
+                        <a href="faq.html" class="btn btn-primary">Learn More!</a>
+                        <br>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8 col-sm-8 meet-our-exec">
+                <div class="panel panel-default text-center">
+                    <div class="panel-body">
+                        <h3>MEET OUR EXEC</h3>
+                        <br>
+                            <img class="img-responsive" src="./img/gt_ifc_example.png" style="height:auto; width:auto; margin:auto; display:block;">
+                        <br>
+                        <a href="exec_board.html" class="btn btn-primary">Learn More!</a>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+        <br>
+        <hr>
+        <br>
+
+        <div class="row">
+            <div class="col-md-7 panel panel-default" style="height:1010px;">
+                <h2 class="page-header" style="color:#FF2400">Latest News</h2>
+                <div id="blog" style="height:884px; margin-bottom:20px; overflow-y:scroll!important;">
+                </div>
+            </div>
+            <div class="col-md-5" style='height:1010px;'>
+                <div class="col-md-6" style="width:100%; height:100%; overflow-y:none; margin-bottom:10px; text-align:center;">
+                    <iframe id="facebook-div-holder" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgtifc%2F&tabs=timeline&width=340&height=1010&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" height="1010" style="border:none;overflow:hidden;resize:horizontal;" scrolling="no" frameborder="0" width="340" allowTransparency="true"></iframe>
+                </div>
+            </div> <!-- col-md-6 above that contains both of these divs -->
+        </div>
+        <!-- /.row -->
