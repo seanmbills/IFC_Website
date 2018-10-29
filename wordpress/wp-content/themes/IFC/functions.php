b200f952f4ae935fb543c90e853e5531245e2c7a
Sean Bills <sbills3@gatech.edu>
2018-10-29 19:30:53 -0400

    Fixed the broken glyphicon elements in things like carousels, as well as fixing the font-awesome elements on the page (like the broken social media elements)

--- wordpress/wp-content/themes/IFC/functions.php
+++ wordpress/wp-content/themes/IFC/functions.php
@@ -10,11 +10,17 @@
         );
     }
 
+    add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+    function enqueue_load_fa() {
+        wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+    }
+
     add_action('init', 'fantom_register_theme_menus');
 
 	function b2w_theme_styles() {
 		wp_enqueue_style('style_css', get_template_directory_uri() . "/style.css");
 		wp_enqueue_style('modern_business', get_template_directory_uri() . "/css/modern-business.css");
+        wp_enqueue_style('bootstrap_min_css', get_template_directory_uri() . "/css/bootstrap.min.css");
 	}
 
 	add_action('wp_enqueue_scripts', 'b2w_theme_styles');
